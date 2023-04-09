a40a6aaf240b4237e7ae40049a90e5cbb99b8f4b
cipher-edu <ciphereduuz@gmail.com>
2023-04-09 13:47:56 +0500

    add  files

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
 # Create your views here.
 def home(request):
     if 'q' in request.GET:
         qidirish = request.GET['q']
-        post = Stock.objects.filter(price__icontains=qidirish)
+        umumiy  = Q(Q(price__icontains=qidirish) | Q(name__icontains=qidirish))
+        post = Stock.objects.filter(umumiy)
     else:
         post = Stock.objects.all()
     # post = Stock.objects.all()
